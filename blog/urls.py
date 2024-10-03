eb59957da406bad786c122c9dec88a95d46601e0
abhishek <abhishek@finestardiamonds.com>
2024-10-03 15:28:45 +0530

    Feat: Blog CRUD Apis implemented

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 # API URL(Routes) Configuration
 urlpatterns = [
-    path('create-blog/', views.create_blog)
+    path('create-blog/', views.create_blog),
+    path('get-blogs/', views.get_blog),
+    path('update-blog/', views.update_blog),
+    path('delete-blog/', views.delete_blog),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import BlogSerializer
 from rest_framework.decorators import api_view
+from .models import Blog
 import datetime
 import uuid
 # Create your views here.
@@ -31,5 +32,90 @@ def create_blog(request):
         Response({
             "title" : "Sever Error",
             "message" : "Unhandled Server Error"
+        }, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+@api_view(['GET'])
+def get_blog(request):
+    try:
+        blogs = Blog.objects.all()
+        serializedData = BlogSerializer(blogs, many = True)
+        if serializedData.data:
+            return Response({
+                    "title" : "All Blogs",
+                    "message" : "Total Active Blogs in the DB.",
+                    "data" : serializedData.data
+                })        
+        return Response({
+            "title" : "No Data Found or Data Invalid",
+            "message" : "No Data was Found or Data is Invalid.",
+        })
+        
+    except Exception as e:
+        return Response({
+            "title" : "Server Error",
+            "message" : "Unhandled Server Error",
+            "error" : str(e)
+        }, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+@api_view(['POST'])
+def update_blog(request):
+    try:
+        if request.data.get('blogId') and request.data.get('title') and request.data.get('content'):
+            try:
+                blog = Blog.objects.get(blogId = request.data.get('blogId'))
+                print('-------------->', blog)
+                blog.title = request.data.get('title')
+                blog.content = request.data.get('content')
+                blog.save()
+                return Response({
+                    "title" : "Blog Updated Successfully",
+                    "message" : "Requested blog updated successfully",
+                    "data" : BlogSerializer(blog, many = False).data
+                })
+            except Exception as e:
+                return Response({
+                    "title" : "Blog not Found",
+                    "message" : "No Blog with the requested Blog Id was found.",
+                    "error" : str(e)
+                })
+        else:
+            return Response({
+                "title" : "Bad Request",
+                "message" : "Bad Request Payload."
+            })
+    except Exception as e:
+        return Response({
+             "title" : "Server Error",
+            "message" : "Unhandled Server Error",
+            "error" : str(e)
+        }, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+@api_view(['DELETE'])
+def delete_blog(request):
+    try:
+        if request.data.get('blogId'):
+            try:
+                blog = Blog.objects.get(blogId = request.data.get('blogId'))
+                blog.delete()
+                return Response({
+                    "title" : "Blog Deleted",
+                    "message" : "Blog Successfully Deleted.",
+                    "blogId" : request.data.get('blogId')
+                })
+            except Exception as e:
+                return Response({
+                    "title" : "Blog Not Found",
+                    "message" : "Blog for requested Blog Id Not Found."
+                }, status = status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({
+                "title" : "Bad Request",
+                "message" : "Bad Request Payload",
+            }, status = status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        return Response({
+            "title" : "Server Error",
+            "message" : "Unhandled Server Error",
+            "error" : str(e)
         }, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
         
