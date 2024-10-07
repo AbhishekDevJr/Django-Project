7bed90cb028acb0775af8e973f01a86ef55b6599
abhishek <abhishek@finestardiamonds.com>
2024-10-07 18:07:48 +0530

    Feat: Student, StudentID, Department models implemented, Added Faker to add mock data to db

--- expense/models.py
+++ expense/models.py
@@ -16,3 +16,33 @@ class Expense(models.Model):
     
     def __str__(self):
         return f"{self.title} {self.expenseId}"
+    
+class Department(models.Model):
+    department = models.CharField(max_length = 100, editable = True)
+    
+    def __str__(self):
+        return f"{self.department}"
+    
+    class Meta:
+        ordering = ['department']
+        
+class StudentID(models.Model):
+    student_id = models.CharField(max_length = 100)
+    
+    def __str__(self):
+        return f"{self.student_id}"
+    
+class Student(models.Model):
+    department = models.ForeignKey(Department, related_name="depart", on_delete = models.CASCADE)
+    student_id = models.OneToOneField(StudentID, related_name = "studentid", on_delete = models.CASCADE)
+    student_name = models.CharField(max_length = 100)
+    student_email = models.EmailField(unique = True)
+    student_age = models.IntegerField(default = 18)
+    student_address = models.TextField()
+    
+    def __str__(self):
+        return f"{self.student_name}"
+    
+    class Meta:
+        ordering = ['student_name']
+        verbose_name = ['student']
